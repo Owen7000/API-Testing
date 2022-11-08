8af2a13009b5b05d7a3d49b5ef4b16dc4204a59d
Owen <77976026+Owen7000@users.noreply.github.com>
2022-11-08 10:24:13 +0000

    Added basic API functionality. Will add more later. I have decided that this will become the API for my web browser and it's extension manager. Thanks, bye for now

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from werkzeug.exceptions import HTTPException
+
+
+app = Flask("app")
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.errorhandler(Exception)
+def error(e):
+  code = 500
+  if isinstance(e, HTTPException):
+    code = e.code
+    return render_template("error.html", e=str(e)), code
+
+@app.route("/api", methods=["GET", "POST"])
+def api():
+	data = request.args.get("data")
+
+	if data is None:
+		return {"type": "Error", "content": "No data requested"}
+	elif data == "1":
+		return {"type": "Data", "content": "Yay! Request successful!"}
+	else:
+		return {"type": "Error", "content": "Data specified does not exist"}
+
+@app.route("/docs")
+def docs():
+  global hits
+  hits += 1
+  return render_template("docs.html")
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=8000)
